bb2a8c3961ac352bf49fbdcb8e414d90906ed3d9
MIGUEL CONCEICAO NUNES <miguel.cnunes@senacsp.edu.br>
2026-02-19 11:12:35 -0300

    Exemplos de parâmetro de corpo; exercicios de parâmetro de corpo; exercicios com mistura de parâmetros; Exercicios parâmetros + Vetores

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import express from "express";
-
 const api = express();
+api.use(express.json());
 
 api.get('/hello', (req, resp) => {
   resp.send('hello word!')  
@@ -29,15 +29,16 @@ api.get('/somar' , (req,resp) =>{
     resp.send ('A soma de ambos é: ' + somar);
 })
 
-api.get('/media/:nota1/:nota2/:nota3', (req, resp) => {
-    let nota1 = Number (req.params.nota1);
-    let nota2 = Number (req.params.nota2);
-    let nota3 = Number(req.params.nota3);
-    let media = Number (nota1 + nota2 + nota3)/3;
-    let situacao = media >= 6 ? 'Aprovado' : 'Reprovado';
+//api.get('/media/:nota1/:nota2/:nota3', (req, resp) => {
+  //  let nota1 = Number (req.params.nota1);
+//     let nota2 = Number (req.params.nota2);
+//    let nota3 = Number(req.params.nota3);
+//    let media = Number (nota1 + nota2 + nota3)/3;
+//   let situacao = media >= 6 ? 'Aprovado' : 'Reprovado';
     
-    resp.send('Média: ' + media + '.Situação: ' + situacao)
-})
+//   resp.send('Média: ' + media + '.Situação: ' + situacao)
+//})
+//_______________________________________________________.._______________________________________________________________________
 api.get('/calculadora/somar/:num1/:num2', (req, resp) => {
     let n1 = Number(req.params.num1);
     let n2 = Number(req.params.num2);
@@ -48,6 +49,7 @@ api.get('/calculadora/somar/:num1/:num2', (req, resp) => {
 }) //http://localhost:5010/calculadora/somar/7/5
 
 
+
 api.get('/calculadora/subtrair', (req, resp) => {
     let n1 = Number(req.query.num1);
     let n2 = Number(req.query.num2);
@@ -56,5 +58,87 @@ api.get('/calculadora/subtrair', (req, resp) => {
             subtracao: sub
     });
 }) //http://localhost:5010/calculadora/subtrair?num1=10&num2=4
+
+
+api.post('/calculadora/multiplicar', (req, resp) => {
+    let dados = req.body;
+    let mult = dados.n1 * dados.n2;
+    resp.send({
+            multiplicacao: mult
+    }) //http://localhost:5010/calculadora/multiplicar
+})
+//opcao 2 
+// let n1 = Number(req.body.n1);
+// let n2 = Number(req.body.n2);
+// let mult = n1 * n2;
+
+//opcao 3 
+//let (n1, n2) = req.body;
+// let mult = n1 * n2;
+
+api.get('/media1/:nota1/:nota2/:nota3', (req, resp) => {
+ let nota1 = Number (req.params.nota1);
+ let nota2 = Number (req.params.nota2);
+ let nota3 = Number(req.params.nota3);
+let media = Number (nota1 + nota2 + nota3)/3;
+let situacao = media >= 6 ? 'Aprovado' : 'Reprovado';
+
+resp.send('Média: ' + media + 'situação: ' + situacao)
+}) //http://localhost:5010/media/10/4/6
+
+api.get('/media2' , (req,resp) =>{
+    let n1 = Number(req.query.n1);
+    let n2 = Number(req.query.n2);
+    let media = (n1 + n2)/2;
+    resp.send ('Média:' + media);
+})// http://localhost:5010/media2?n1=7&n2=10
+
+api.post('/media3', (req, resp) => {
+
+    let media3 = (req.body.n1 + req.body.n2 + req.body.n3)/3;
+    let situacao = media3 >= 6 ? 'Aprovado' : 'Reprovado';
+    resp.send({
+        media: media3,
+        situacao: situacao
+    })//http://localhost:5010/media3
+})
+
+api.post ('/media4/:n1', (req, resp) => {
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.query.n2);
+
+    let dados = req.body;
+    let n3 = Number(dados.n3);
+    let media =(n1 + n2 + n3)/3;
+    let situacao = media >= 6 ? 'Aprovado' : 'Reprovado';
+    resp.send({
+        media: media,
+        situacao: situacao})
+    })//http://localhost:5010/media4/5?n2=8
+
+
+    api.post('/media/alunos/relatorio', (req, resp) => {
+        let listaAlunos = req.body.alunos;
+    
+        let relatorio = listaAlunos.map(aluno => {
+            // Calcula a soma das notas do aluno atual
+            let soma = aluno.notas.reduce((acc, nota) => acc + nota, 0);
+            
+            // Calcula a média
+            let media = soma / aluno.notas.length;
+            
+            // Retorna um novo objeto com o nome, média formatada e situação
+            return {
+                nome: aluno.nome,
+                media: Number(media.toFixed(1)),
+                situacao: media >= 6 ? 'Aprovado' : 'Reprovado'
+            };
+        });
+    
+        resp.send(relatorio);
+    });
+
+
+
 
 api.listen(5010, ()=> console.log('API subiu!'));
